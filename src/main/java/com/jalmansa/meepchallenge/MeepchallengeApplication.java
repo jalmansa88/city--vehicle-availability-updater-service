65d19af9fa32600930e3db056f3f2c42a2358c30
jalmansa <jalmansa88@gmail.com>
2020-11-14 10:11:27 +0100

    created project scaffolding

---
+++ src/main/java/com/jalmansa/meepchallenge/MeepchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.jalmansa.meepchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeepchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeepchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jalmansa/meepchallenge/MeepchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalmansa.meepchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeepchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
